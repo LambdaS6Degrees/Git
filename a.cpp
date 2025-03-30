7fb9ee209e9341c036acc54c89fed9f0d79d73b0
LambdaS6Degrees <wanquanmeiyou@gmail.com>
2025-03-30 11:57:08 +0800

    another one

--- a.cpp
+++ a.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
-
+#include <iostream>
 int main()
 {
+	std::cout<<"git"<<std::endl;
 	return 0;
 }
---
+++ b.cpp
@@ -0,0 +1,13 @@
+#include <memory>
+#include <iostream>
+void changeNum(std::unique_ptr<int>&& pi)
+{
+    *pi = 29;
+}
+
+void test()
+{
+    std::unique_ptr<int> inum = std::make_unique<int>(int(12));
+    changeNum(std::move(inum));
+    std::cout<<*inum<<std::endl;
+}
